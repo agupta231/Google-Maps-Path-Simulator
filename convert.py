a06cacbf2f230cf473d4d963e47f04f7f8d3e299
Ankur Gupta <rukna1000@gmail.com>
2018-04-19 00:46:38 -0400

    Got it it to work with multithreading

--- convert.py
+++ convert.py
@@ -1,7 +1,17 @@
+from multiprocessing.dummy import Pool as ThreadPool
 import googlemaps
 import argparse
 import json
 import sys
+
+with open('key', 'r') as fh:
+    key = fh.readlines()[0].strip()
+
+gmaps = googlemaps.Client(key=key)
+
+def get_elevation(location):
+    elevation = gmaps.elevation(location)[0]
+    return elevation['elevation']
 
 parser = argparse.ArgumentParser(description="""Google Maps Path Simulators: a utility to
         help simulate bike paths on stationary bikes.""")
@@ -25,27 +35,24 @@ parser.add_argument("--flat_ground_resistance_level",
 
 args = parser.parse_args()
 
-with open('key', 'r') as fh:
-    key = fh.readlines()[0].strip()
-
-gmaps = googlemaps.Client(key=key)
-
 elevation = gmaps.elevation((42.274720000,-71.806640000))[0]
 print(elevation['elevation'])
 
 coordinates = []
-elevations = []
 
 with open(args.url, "r") as fh:
     for line in fh.readlines():
         line = line.strip().split(",")
 
         coordinate = (float(line[1]), float(line[2]))
-        elevation = gmaps.elevation(coordinate)[0]
-        print(elevation)
-
         coordinates.append(coordinate)
-        elevations.append(elevation['elevation'])
+
+pool = ThreadPool(8)
+
+elevations = pool.map(get_elevation, coordinates)
+
+pool.close()
+pool.join()
 
 print(coordinates)
 print(elevations)
